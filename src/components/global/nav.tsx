5b48c80b852fb8b35468123aa224fe4bd5b63f8c
Richard Hofmänner <hello@terillos.dev>
2021-04-04 22:27:20 +0200

    Added Home Route

--- src/components/global/nav.tsx
+++ src/components/global/nav.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Nav = () => {
   const navItems = [
-    {path: '/', text: 'Home'}
+    {path: '/', text: 'Home'},
+    {path: '/about', text: 'About'}
   ];
   return (
-    <div className="container mx-auto">
-      <nav>
+    <div className="container mx-auto mb-10">
+      <nav role="navigation">
         <ul className="mt-10">
           {navItems.map((item, index) => {
               return (
-                <li key={index}>
-                  <Link to={item.path}>{item.text}</Link>
+                <li
+                  key={index}
+                  className="inline-block"
+                >
+                  <NavLink
+                    className="px-5 py-2 transition-colors hover:text-gray-400"
+                    to={item.path}
+                    activeClassName="text-gray-400"
+                  >
+                    {item.text}
+                  </NavLink>
                 </li>
               )
             })}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,12 @@ import React, {Component} from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="container mx-auto">
+        <h1>Welcome</h1>
+        <p>
+          This is a simple web app which you can use to browse the spacex api (currently v4). I made this to experiment
+          a little bit with React. To view the different data, just click on a link above in the navigation.
+        </p>
       </div>
     );
   }
